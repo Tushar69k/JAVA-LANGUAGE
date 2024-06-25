2b0b03a3c95ae63e896042e2c2be9a4a918f05af
TUSHAR KHARADE <tushlappy@gmail.com>
2024-06-26 02:38:36 +0530

    Progress of 25-June-2024

--- src/PracticeWork/Progress/SortingAlgorithm/CountingSort__4.java
+++ src/PracticeWork/Progress/SortingAlgorithm/CountingSort__4.java
@@ -6,8 +6,10 @@ public class CountingSort__4 {
     public static void main(String[] args) {
         Scanner tush = new Scanner(System.in);
 
-        int[] arr = Setarr();
+//        int[] arr = Setarr();
+        int[] arr = {1, 4, 1, 3, 2, 4, 3, 7};
         GetArr(arr);
+        SelectionSort(arr);
     }
 
     public static int[] Setarr() {
@@ -28,7 +30,51 @@ public class CountingSort__4 {
     public static void GetArr(int[] arr) {
         System.out.println("Elements in Array Are :: ");
         for (int i = 0; i < arr.length; i++) {
-            System.out.println("Element [" + (i + 1) + "] = " + arr[i]);
+            System.out.print(" | " + arr[i]);
         }
+        System.out.println(" | ");
+
+    }
+
+    public static void SelectionSort(int[] arr) {
+        Scanner tush = new Scanner(System.in);
+
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+
+        int count = 0;
+
+        int[] arr2 = new int[largest + 1];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (i == arr[j]) {
+                    count = count + 1;
+                }
+                arr2[i] = count;
+            }
+            count = 0;
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(" | " + arr2[i]);
+        }
+        System.out.println(" | ");
+
+        int j = 0;
+        for (int i = 0; i < arr2.length; i++) {
+            while (arr2[i] > 0) {
+                arr[j] = i;
+                j++;
+                arr2[i]--;
+            }
+        }
+
+        System.out.println(" Counting Sort :: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(" | " + arr[i]);
+        }
+        System.out.println(" | ");
     }
 }
