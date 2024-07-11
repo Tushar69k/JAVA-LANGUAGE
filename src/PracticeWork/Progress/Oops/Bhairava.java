691096e0bb5339594d52168a8a34e5c2c815c06a
TUSHAR KHARADE <tushlappy@gmail.com>
2024-07-11 18:55:17 +0530

    Progress of 11-July-2024

--- src/PracticeWork/Progress/Oops/Bhairava.java
+++ src/PracticeWork/Progress/Oops/Bhairava.java
@@ -3,4 +3,13 @@ package PracticeWork.Progress.Oops;
 public class Bhairava {
     String car = "Bhujji";
     String city = "Complex";
+    private int oye;
+
+    public void Setnum(int n) { // Setter Created
+        oye = n;
+    }
+
+    public int Getnum() { // Getter Created
+        return oye;
+    }
 }
