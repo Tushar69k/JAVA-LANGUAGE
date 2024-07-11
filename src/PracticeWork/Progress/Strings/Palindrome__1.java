cb104db270c58dd300b394637c434bac56b67716
TUSHAR KHARADE <tushlappy@gmail.com>
2024-07-11 12:16:50 +0530

    Progress of 11-July-2024

---
+++ src/PracticeWork/Progress/Strings/Palindrome__1.java
@@ -0,0 +1,44 @@
+package PracticeWork.Progress.Strings;
+
+import java.nio.file.FileVisitOption;
+import java.util.*;
+
+public class Palindrome__1 {
+    public static void main(String[] args) {
+        System.out.println("Program to check String is Palindrome or Not :: ");
+        Scanner tush = new Scanner(System.in);
+        System.out.print("Enter your String : ");
+        String str = tush.nextLine();
+        Reverse(str);
+        Palindrome(str);
+    }
+
+    public static void Palindrome(String str) {
+        int n = str.length();
+        int flag = 0;
+        for (int i = 0; i < (n / 2); i++) {
+            if (str.charAt(i) == str.charAt(n - i - 1)) {
+                flag = 1;
+            }
+        }
+
+        if (flag != 1) {
+            System.out.println("String is not Palindrome.");
+        } else if (flag == 1) {
+            System.out.println("String is Palindrome!");
+        }
+    }
+
+
+    public static void Reverse(String str) {
+        String temp = " ";
+        int n = str.length();
+        char ch;
+        for (int i = n - 1; i >= 0; i--) {
+            ch = str.charAt(i);
+            temp += ch;
+        }
+        System.out.println("Reversed String :" + temp);
+    }
+
+}
