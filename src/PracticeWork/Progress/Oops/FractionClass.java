20e505009ff9f4c681e3c9d09cb73d8ce6a8a2c1
Tushar69k <tushlappy@gmail.com>
2024-07-13 21:46:35 +0530

    Progress of 13-July-2024

---
+++ src/PracticeWork/Progress/Oops/FractionClass.java
@@ -0,0 +1,17 @@
+package PracticeWork.Progress.Oops;
+
+public class FractionClass {
+
+    int num;
+    int dem;
+
+    public FractionClass(int num, int dem) {
+        this.num = num;
+        this.dem = dem;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
