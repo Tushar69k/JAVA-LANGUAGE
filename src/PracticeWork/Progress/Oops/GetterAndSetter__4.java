95fd2de72a8e747d2b0308a44ed87d3126a85f01
Tushar69k <tushlappy@gmail.com>
2024-07-13 21:29:49 +0530

    Progress of 13-July-2024

---
+++ src/PracticeWork/Progress/Oops/GetterAndSetter__4.java
@@ -0,0 +1,20 @@
+package PracticeWork.Progress.Oops;
+
+import java.util.*;
+
+public class GetterAndSetter__4 {
+    public static void main(String[] args) {
+
+        Scanner tush = new Scanner(System.in);
+        Bhairava t = new Bhairava();
+        System.out.println(t.car);
+        System.out.println(t.city);
+
+        t.Setnum(69); // Setter Called
+        // Setting 69 value using Setter Function
+
+        System.out.println(t.Getnum()); // Getter Called
+        // Getting value through Getter Function
+
+    }
+}
