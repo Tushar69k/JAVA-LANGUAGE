5808d8930fd293aa788ba676962a770f4679a196
Tushar69k <tushlappy@gmail.com>
2024-07-19 16:53:34 +0530

    Progress of 19-July-2024

---
+++ src/PracticeWork/Progress/Oops/Classes/Constructors/TypesOfConstructors.java
@@ -0,0 +1,39 @@
+/*
+Constructors are Special Member Function which are called when an Object is Created .
+They are used to Initialise values to Data Members of Objects.
+
+Types Of Constructor :-
+1. Default / Non-Parameterised Constructor
+2. Parameterised Constructor
+3. Copy Constructor
+
+1. Default / Non-Parameterised Constructor :-
+        These do not require any Parameter and uses Default values to initialise object
+        and can be created Implicitly by the Compiler Itself if no Default Constructor
+        is Given to it.
+
+2. Parameterised Constructor
+        These Constructors uses Argument to Initialise Values to Data Members of object.
+        We can Give different types of Parameter to the Constructors and this overloading
+        of Constructors is known as Constructor Overloading.
+        ( This is known as Polymorphism )
+
+3. Copy Constructor
+        These are used to Copy Data Members of one Object to Another Object.
+
+
+Java Garbage Collectors are used to Free up Memory at Runtime
+This Concept is not in C++ , Hence we Create Destructors There
+
+*/
+package PracticeWork.Progress.Oops.Classes.Constructors;
+
+import java.util.*;
+
+public class TypesOfConstructors {
+    public static void main(String[] args) {
+        Scanner tush = new Scanner(System.in);
+
+
+    }
+}
