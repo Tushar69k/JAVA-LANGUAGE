a3a899c55f46cd1948bd99e654bb6dc62cd60afe
Tushar69k <tushlappy@gmail.com>
2024-07-20 11:50:33 +0530

    Progress of 20-July-2024

---
+++ src/PracticeWork/Progress/Oops/Classes/Inheritance/Hierarchical.java
@@ -0,0 +1,47 @@
+package PracticeWork.Progress.Oops.Classes.Inheritance;
+
+import java.util.*;
+
+public class Hierarchical {
+    public static void main(String[] args) {
+        Scanner tush = new Scanner(System.in);
+
+        Fish Nemo = new Fish();
+        Nemo.Swim();
+        Nemo.Eats();
+        Nemo.Breaths();
+
+        System.out.println();
+        Bird Red = new Bird();
+        Red.Fly();
+        Red.Eats();
+        Red.Breaths();
+    }
+
+    static class Animal {
+        void Eats() {
+            System.out.println("Eating...");
+        }
+
+        void Breaths() {
+            System.out.println("Breathing...");
+        }
+    }
+
+    static class Fish extends Animal {
+        void Swim() {
+            System.out.println("Fish Swims...");
+        }
+    }
+
+    static class Bird extends Animal {
+        void Fly() {
+            System.out.println("Bird Fly...");
+        }
+
+        // Animal Class is Hierarchical Inherited to Bird and Fish Class
+
+    }
+
+
+}
