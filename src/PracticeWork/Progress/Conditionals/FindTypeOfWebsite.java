4cc0498a9743def96603273032158f8ebf37afdd
Tushar69k <tushlappy@gmail.com>
2024-07-24 14:46:02 +0530

    Progress of 24-July-2024

--- src/PracticeWork/Progress/Conditionals/FindTypeOfWebsite.java
+++ src/PracticeWork/Progress/Conditionals/FindTypeOfWebsite.java
@@ -3,7 +3,7 @@ package PracticeWork.Progress.Conditionals;
 
 import java.util.*;
 
-public class FIndTypeOfWebsite__4 {
+public class FindTypeOfWebsite {
     public static void main(String[] args) {
         Scanner tush = new Scanner(System.in);
         System.out.print("Enter Website : ");
