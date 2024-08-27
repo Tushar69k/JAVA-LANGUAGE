a8a920177b916ea34c6871b593adaf54e82d6495
Tushar69k <tushlappy@gmail.com>
2024-08-27 20:08:18 +0530

    Progress of 27-August-2024

---
+++ src/PracticeWork/Progress/Oops/HUHUHUHUHUHU/FractionMain__6.java
@@ -0,0 +1,9 @@
+package PracticeWork.Progress.Oops.HUHUHUHUHUHU;
+
+public class FractionMain__6 {
+    public static void main(String[] args) {
+
+        FractionClass fc = new FractionClass(3, 4);
+        System.out.println(fc.num + "/" + fc.dem);
+    }
+}
