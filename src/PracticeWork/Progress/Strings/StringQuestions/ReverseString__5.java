78d185e335b2c601e657e2b813e55ccf07f39056
Tushar69k <tushlappy@gmail.com>
2024-08-28 22:30:30 +0530

    Progress of 28-August-2024

---
+++ src/PracticeWork/Progress/Strings/StringQuestions/ReverseString__5.java
@@ -0,0 +1,25 @@
+package PracticeWork.Progress.Strings.StringQuestions;
+
+import java.util.*;
+
+public class ReverseString__5 {
+    public static void main(String[] args) {
+        Scanner tush = new Scanner(System.in);
+        String str;
+
+//    System.out.print(" Enter String : ");
+//        str = tush.nextLine();
+//        int first = 0, last = str.length() - 1;
+//
+//        while (first < last) {
+//            int temp = str[last];
+//            arr[last] = arr[first];
+//            arr[first] = temp;
+//
+//            first++;
+//            last--;
+//        }
+
+
+    }
+}
