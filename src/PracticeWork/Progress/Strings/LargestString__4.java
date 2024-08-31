4ed3570f2f3b25e5f4b95c22a7b6e7953d9f97a5
Tushar69k <tushlappy@gmail.com>
2024-08-31 11:10:07 +0530

    Progress of 31-August-2024

---
+++ src/PracticeWork/Progress/Strings/LargestString__4.java
@@ -0,0 +1,30 @@
+package PracticeWork.Progress.Strings;
+
+import java.util.*;
+
+public class LargestString__4 {
+
+    public static void main(String[] args) {
+        Scanner tush = new Scanner(System.in);
+        String[] fruits = {"apple", "mango", "strawberry", "banana", "dragonfruit"};
+
+        System.out.print("Our String is : ");
+        for (int i = 0; i < fruits.length; i++) {
+            System.out.print(fruits[i] + " , ");
+        }
+        System.out.print("etc.");
+        System.out.println();
+
+
+        String largest = fruits[0];
+        for (int i = 1; i < fruits.length; i++) {
+            if (largest.compareTo(fruits[i]) < 0) {
+                largest = fruits[i];
+            }
+        }
+
+        System.out.println("Largest String is : " + largest);
+    }
+
+
+}
