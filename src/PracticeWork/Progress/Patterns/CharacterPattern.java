9ab41c9bcb7fd83b67a425c15275e44fda192df1
Tushar69k <tushlappy@gmail.com>
2024-09-09 20:27:33 +0530

    Progress of 09-September-2024

--- src/PracticeWork/Progress/Patterns/CharacterPattern.java
+++ src/PracticeWork/Progress/Patterns/CharacterPattern.java
@@ -3,7 +3,7 @@ package PracticeWork.Progress.Patterns;
 import java.util.*;
 
 
-public class CharacterPattern__3 {
+public class CharacterPattern {
     public static void main(String[] args) {
         Scanner tush = new Scanner(System.in);
         System.out.print("Enter Size : ");
