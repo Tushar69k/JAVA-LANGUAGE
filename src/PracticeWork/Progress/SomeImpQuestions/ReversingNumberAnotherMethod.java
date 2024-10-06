504f729a7dce047895aaf746f156c88c8f4bbb33
Tushar69k <tushlappy@gmail.com>
2024-10-06 12:16:21 +0530

    Progress of 06-October-2024

--- src/PracticeWork/Progress/SomeImpQuestions/ReversingNumberAnotherMethod.java
+++ src/PracticeWork/Progress/SomeImpQuestions/ReversingNumberAnotherMethod.java
@@ -3,7 +3,7 @@ package PracticeWork.Progress.SomeImpQuestions;
 
 import java.util.*;
 
-public class ReversingNumberAnotherMethod__4 {
+public class ReversingNumberAnotherMethod {
     public static void main(String[] args) {
         Scanner tush = new Scanner(System.in);
         System.out.println("Enter number : ");
