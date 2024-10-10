fe9b2e9819ea23bad52bccb714c856fa4cf553c3
Tushar69k <tushlappy@gmail.com>
2024-10-10 09:22:48 +0530

    Progress of 10-October-2024

--- src/PracticeWork/Progress/Strings/ToggleCharacter.java
+++ src/PracticeWork/Progress/Strings/ToggleCharacter.java
@@ -5,7 +5,7 @@ package PracticeWork.Progress.Strings;
 
 import java.util.*;
 
-public class ToggleCharacter__10 {
+public class ToggleCharacter {
     public static void main(String[] args) {
         Scanner tush = new Scanner(System.in);
         StringBuilder str = new StringBuilder(SetString());
