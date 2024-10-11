6978717f72a1694d14b07099ac0c08a92aa4c43a
Tushar69k <tushlappy@gmail.com>
2024-10-11 12:38:29 +0530

    Progress of 11-October-2024

--- src/PracticeWork/Progress/Strings/StringQuestions/DetectSpaces.java
+++ src/PracticeWork/Progress/Strings/StringQuestions/DetectSpaces.java
@@ -3,7 +3,7 @@ package PracticeWork.Progress.Strings.StringQuestions;
 
 import java.util.*;
 
-public class DetectSpaces__4 {
+public class DetectSpaces {
     public static void main(String[] args) {
         Scanner tush = new Scanner(System.in);
         System.out.print("Enter any String : ");
