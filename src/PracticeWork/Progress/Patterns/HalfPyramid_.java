0a420b65ae0ebfef9d2eb61db6c627ad89d7409c
Tushar69k <tushlappy@gmail.com>
2024-10-15 21:32:50 +0530

    Progress of 15-October-2024

--- src/PracticeWork/Progress/Patterns/HalfPyramid_.java
+++ src/PracticeWork/Progress/Patterns/HalfPyramid_.java
@@ -2,7 +2,7 @@ package PracticeWork.Progress.Patterns;
 
 import java.util.*;
 
-public class HalfPyramid {
+public class HalfPyramid_ {
     public static void main(String[] args) {
         Scanner tush = new Scanner(System.in);
         System.out.print("Enter Size : ");
