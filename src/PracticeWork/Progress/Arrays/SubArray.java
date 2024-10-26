cf805fd5be6c7e97692f8e67c0cf5e6ca672eb76
Tushar69k <tushlappy@gmail.com>
2024-10-27 00:01:27 +0530

    Progress of 25-October-2024

--- src/PracticeWork/Progress/Arrays/SubArray.java
+++ src/PracticeWork/Progress/Arrays/SubArray.java
@@ -2,7 +2,7 @@ package PracticeWork.Progress.Arrays;
 
 import java.util.*;
 
-public class SubArray_ {
+public class SubArray {
     public static void main(String[] args) {
         Scanner tush = new Scanner(System.in);
         System.out.print("Enter Size of Array : ");
