1c98b228a7305422b494f3d156770e7d5abadd8f
Tushar69k <tushlappy@gmail.com>
2024-11-04 20:05:37 +0530

    Progress of 3-November-2024

--- src/FunctionsUsage/String/SubString.java
+++ src/FunctionsUsage/String/SubString.java
@@ -10,7 +10,7 @@ substring(i); --> gives substring from index i to end of string
 
 package FunctionsUsage.String;
 
-public class SubString_ {
+public class SubString {
     public static void main(String[] args) {
         String s = "abcd";
         System.out.print(" " + s.substring(0, 1));
