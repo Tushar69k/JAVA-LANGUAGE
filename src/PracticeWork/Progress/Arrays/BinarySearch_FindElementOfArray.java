7afbcdf846e76f66c0b83e16645c3f8c79651e61
Tushar69k <tushlappy@gmail.com>
2024-11-07 12:53:11 +0530

    Progress of 7-November-2024

--- src/PracticeWork/Progress/Arrays/BinarySearch_FindElementOfArray.java
+++ src/PracticeWork/Progress/Arrays/BinarySearch_FindElementOfArray.java
@@ -2,7 +2,7 @@ package PracticeWork.Progress.Arrays;
 
 import java.util.*;
 
-public class BinarySearch_FindElementOfArray_ {
+public class BinarySearch_FindElementOfArray {
     public static void main(String[] args) {
         Scanner tush = new Scanner(System.in);
         System.out.print("Enter Size of Array : ");
