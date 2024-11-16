c0365ff0bb0a5910d4490ce35b104f8a04766b18
Tushar69k <tushlappy@gmail.com>
2024-11-16 12:34:30 +0530

    Progress of 16-November-2024

--- src/PracticeWork/Progress/Arrays/BasicArrayQuestions/SortedOrNot_.java
+++ src/PracticeWork/Progress/Arrays/BasicArrayQuestions/SortedOrNot_.java
@@ -5,7 +5,7 @@ package PracticeWork.Progress.Arrays.BasicArrayQuestions;
 
 import java.util.*;
 
-public class SortedOrNot {
+public class SortedOrNot_ {
     public static void main(String[] args) {
         Scanner tush = new Scanner(System.in);
 
