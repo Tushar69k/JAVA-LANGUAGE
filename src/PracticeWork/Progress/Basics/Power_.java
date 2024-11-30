7add0145738d0b9de5a7a80e56085b9f659deca9
Tushar69k <tushlappy@gmail.com>
2024-11-30 06:26:52 +0530

    Progress of 29-November-2024

--- src/PracticeWork/Progress/Basics/Power_.java
+++ src/PracticeWork/Progress/Basics/Power_.java
@@ -4,7 +4,7 @@ package PracticeWork.Progress.Basics;
 
 import java.util.Scanner;
 
-public class Power {
+public class Power_ {
 
     public static void main(String args[]) {
         System.out.println("Program to Calculate Square  :: ");
