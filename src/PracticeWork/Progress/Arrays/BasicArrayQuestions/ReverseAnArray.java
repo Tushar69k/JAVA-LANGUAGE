9c0bc8a9fb3042bbc10e9350eb829c85f29da8a3
Tushar69k <tushlappy@gmail.com>
2024-12-05 08:07:12 +0530

    Progress of 05-November-2024

--- src/PracticeWork/Progress/Arrays/BasicArrayQuestions/ReverseAnArray.java
+++ src/PracticeWork/Progress/Arrays/BasicArrayQuestions/ReverseAnArray.java
@@ -4,7 +4,7 @@ package PracticeWork.Progress.Arrays.BasicArrayQuestions;
 import java.util.*;
 
 
-public class ReverseAnArray_ {
+public class ReverseAnArray {
     public static void main(String[] args) {
         Scanner tush = new Scanner(System.in);
 
