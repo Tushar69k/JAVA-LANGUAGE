fcd19c6214174a0822335c3119722403559748af
Tushar69k <tushlappy@gmail.com>
2024-12-07 00:11:15 +0530

    Progress of 06-November-2024

--- src/PracticeWork/Progress/Strings/StringBuilder__5.java
+++ src/PracticeWork/Progress/Strings/StringBuilder__5.java
@@ -1,33 +1,33 @@
-package PracticeWork.Progress.Strings;
-
-import java.util.*;
-
-public class StringBuilder {
-    public static void main(String[] args) {
-        Scanner tush = new Scanner(System.in);
-        String str = SetString();
-        GetString(str);
-        StringBuilder(str);
-    }
-
-    public static String SetString() {
-        Scanner tush = new Scanner(System.in);
-        System.out.print("Enter String : ");
-        String str = tush.nextLine();
-        return str;
-    }
-
-    public static void GetString(String str) {
-        System.out.print("Entered String is : " + str);
-    }
-
-
-    public static void StringBuilder(String str) {
-        StringBuilder sb = new StringBuilder("");
-        for (char ch = 'a'; ch <= 'z'; ch++) {
-            sb.append(ch);
-        }
-        System.out.println("\nString Builder ::");
-        System.out.println(sb);
-    }
-}
+//package PracticeWork.Progress.Strings;
+//
+//import java.util.*;
+//
+//public class StringBuilder {
+//    public static void main(String[] args) {
+//        Scanner tush = new Scanner(System.in);
+//        String str = SetString();
+//        GetString(str);
+//        StringBuilder(str);
+//    }
+//
+//    public static String SetString() {
+//        Scanner tush = new Scanner(System.in);
+//        System.out.print("Enter String : ");
+//        String str = tush.nextLine();
+//        return str;
+//    }
+//
+//    public static void GetString(String str) {
+//        System.out.print("Entered String is : " + str);
+//    }
+//
+//
+//    public static void StringBuilder(String str) {
+//        StringBuilder sb = new StringBuilder("");
+//        for (char ch = 'a'; ch <= 'z'; ch++) {
+//            sb.append(ch);
+//        }
+//        System.out.println("\nString Builder ::");
+//        System.out.println(sb);
+//    }
+//}
