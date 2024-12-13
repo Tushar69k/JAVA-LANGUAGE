2ae73aa49b5f0ef96e2e224f0ee84c51ce1619bd
Tushar69k <tushlappy@gmail.com>
2024-12-13 23:42:21 +0530

    Progress of 12-December-2024

---
+++ src/PracticeWork/Progress/Afterwards/Conditional_.java
@@ -0,0 +1,26 @@
+package PracticeWork.Progress.Afterwards;
+
+import java.util.Scanner;
+
+public class Conditional_ {
+    public static void main(String[] args) {
+        Scanner tush = new Scanner(System.in);
+        System.out.println("Enter the No. of your day : ");
+        int num = tush.nextInt();
+
+        switch (num) {
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
+
+        }
+
+
+    }
+
+
+}
