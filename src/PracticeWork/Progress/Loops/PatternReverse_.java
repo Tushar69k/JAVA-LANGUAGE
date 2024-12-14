d4e41a5e4d110ed2e01bef8503ac2b334880e114
Tushar69k <tushlappy@gmail.com>
2024-12-14 18:12:08 +0530

    Progress of 14-December-2024

--- src/PracticeWork/Progress/Loops/PatternReverse_.java
+++ src/PracticeWork/Progress/Loops/PatternReverse_.java
@@ -2,7 +2,7 @@ package PracticeWork.Progress.Loops;
 
 import java.util.*;
 
-public class PatternReverse {
+public class PatternReverse_ {
     public static void main(String[] args) {
         Scanner tush = new Scanner(System.in);
         System.out.println("Enter Number : ");
