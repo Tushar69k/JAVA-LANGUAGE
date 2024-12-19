e390521ef8e3747f2a325af03df7a4f33695c7e6
Tushar69k <tushlappy@gmail.com>
2024-12-19 18:09:38 +0530

    Progress of 19-December-2024

--- src/PracticeWork/Progress/Afterwards/Conditional.java
+++ src/PracticeWork/Progress/Afterwards/Conditional.java
@@ -2,7 +2,7 @@ package PracticeWork.Progress.Afterwards;
 
 import java.util.Scanner;
 
-public class Conditional_ {
+public class Conditional {
     public static void main(String[] args) {
         Scanner tush = new Scanner(System.in);
         System.out.println("Enter the No. of your day : ");
