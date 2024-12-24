bfb4e830735b8f8e14ec3e645f0898e15ee84d90
Tushar69k <tushlappy@gmail.com>
2024-12-24 23:34:03 +0530

    Progress of 20-December-2024

--- src/PracticeWork/Progress/Afterwards/LoopsFactorial_.java
+++ src/PracticeWork/Progress/Afterwards/LoopsFactorial_.java
@@ -2,7 +2,7 @@ package PracticeWork.Progress.Afterwards;
 
 import java.util.Scanner;
 
-public class LoopsFactorial {
+public class LoopsFactorial_ {
     public static void main(String[] args) {
         Scanner tush = new Scanner(System.in);
 
