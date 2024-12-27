9b0343de90d5db542a63564ccc8332168822663b
Tushar69k <tushlappy@gmail.com>
2024-12-27 11:30:55 +0530

    Progress of 20-December-2024

--- src/PracticeWork/Progress/Afterwards/okokjkkk.java
+++ src/PracticeWork/Progress/Afterwards/okokjkkk.java
@@ -2,7 +2,7 @@ package PracticeWork.Progress.Afterwards;
 
 import java.util.Scanner;
 
-public class okokj {
+public class okokjkkk {
     public static void main(String[] args) {
         Scanner tush = new Scanner(System.in);
         System.out.println("Enter the No. of your day : ");
